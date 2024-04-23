3831c2105306181571dcd592305803c58ab0a24b
Thural Musaibov <tural.musaibov@outlook.com>
2024-04-23 21:32:37 +0400

    initial commit

---
+++ src/test/java/com/jellybrains/quietspace_backend_ms/QuietspaceBackendMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jellybrains.quietspace_backend_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuietspaceBackendMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
