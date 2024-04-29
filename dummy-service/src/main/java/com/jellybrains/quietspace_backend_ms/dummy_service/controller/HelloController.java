ea2a3bc19913dabc6fb4c920776f753d436f8033
Thural Musaibov <tural.musaibov@outlook.com>
2024-04-29 16:36:43 +0400

    implement performance based resiliency

--- dummy-service/src/main/java/com/jellybrains/quietspace_backend_ms/dummy_service/controller/HelloController.java
+++ dummy-service/src/main/java/com/jellybrains/quietspace_backend_ms/dummy_service/controller/HelloController.java
@@ -1,23 +1,34 @@
 package com.jellybrains.quietspace_backend_ms.dummy_service.controller;
 
 import com.jellybrains.quietspace_backend_ms.dummy_service.dto.request.DummyRequest;
+import com.jellybrains.quietspace_backend_ms.dummy_service.service.DummyService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/dummy")
 public class HelloController {
 
+    private final DummyService dummyService;
+
     @GetMapping("/hello")
     @CircuitBreaker(name = "dummy", fallbackMethod = "fallbackMethod")
-    public String hello(@RequestBody DummyRequest dummyRequest) {
-        return "Hello " + dummyRequest.getName();
+    @TimeLimiter(name = "dummy")
+    @Retry(name = "dummy")
+    public CompletableFuture<String> hello(@RequestBody DummyRequest dummyRequest) {
+        return CompletableFuture.supplyAsync(() -> dummyService.buildMessage(dummyRequest));
     }
 
-    public String fallbackMethod(DummyRequest dummyRequest, RuntimeException runtimeException) {
-        return "Something went wrong, please try later";
+    public CompletableFuture<String> fallbackMethod(DummyRequest dummyRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Something went wrong, please try later");
     }
 }
---
+++ dummy-service/src/main/java/com/jellybrains/quietspace_backend_ms/dummy_service/service/DummyService.java
@@ -0,0 +1,19 @@
+package com.jellybrains.quietspace_backend_ms.dummy_service.service;
+
+import com.jellybrains.quietspace_backend_ms.dummy_service.dto.request.DummyRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class DummyService {
+
+    @SneakyThrows
+    public String buildMessage(DummyRequest request){
+        log.info("Wait started");
+        Thread.sleep(10*1000);
+        log.info("Wait ended");
+        return "Hello " + request.getName();
+    }
+}
