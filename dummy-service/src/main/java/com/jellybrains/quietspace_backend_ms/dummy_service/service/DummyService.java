83014dd5773e2dab0c36928eb97e1b281d717785
Thural Musaibov <tural.musaibov@outlook.com>
2024-05-02 21:11:28 +0400

    reconfigure appplication properties external variables

--- dummy-service/src/main/java/com/jellybrains/quietspace_backend_ms/dummy_service/service/DummyService.java
+++ dummy-service/src/main/java/com/jellybrains/quietspace_backend_ms/dummy_service/service/DummyService.java
@@ -12,7 +12,7 @@ public class DummyService {
     @SneakyThrows
     public String buildMessage(DummyRequest request){
         log.info("Wait started");
-        Thread.sleep(10*1000);
+        Thread.sleep(11*1000);
         log.info("Wait ended");
         return "Hello " + request.getName();
     }
